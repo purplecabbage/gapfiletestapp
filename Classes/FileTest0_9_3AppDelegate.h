b82f4817411dcb2d33295f373c8a15a39659740b
Jesse <jesse.macfadyen@nitobi.com>
2011-01-31 14:41:15 -0800

    Test the file api from a dialog

---
+++ Classes/FileTest0_9_3AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  FileTest0_9_3AppDelegate.h
+//  FileTest0.9.3
+//
+//  Created by Jesse MacFadyen on 10-11-29.
+//  Copyright Nitobi 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface FileTest0_9_3AppDelegate : PhoneGapDelegate {
+}
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FileTest0.9.3
+//
+//  Created by Jesse MacFadyen on 10-11-29.
+//  Copyright Nitobi 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"FileTest0_9_3AppDelegate");
+    [pool release];
+    return retVal;
+}
